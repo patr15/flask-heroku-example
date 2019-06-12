42238e828f14e2b654909ccc8ff3996d6f3771a3
patr15 <51729322+patr15@users.noreply.github.com>
2019-06-12 10:07:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    json_data = {'Hello': 'World!'}
-    return jsonify(json_data)
-
+   # json_data = {'Hello': 'World!'}
+   # return ปณิตา 8 4/1
+     return 
 
 if __name__ == '__main__':
     app.run()
